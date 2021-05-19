1f63148998be261309c83280ce4f9783ec87a0f3
carlos.iglesiasgomez <carlos.iglesiasgomez@wirtz.local>
2021-05-19 18:19:57 +0200

    dale

--- src/main/java/ejercicios/Ejercicio2.java
+++ src/main/java/ejercicios/Ejercicio2.java
@@ -1,5 +1,17 @@
 package ejercicios;
 
+/**
+ * 9.2. Realiza un programa que permita al usuario seleccionar un tipo de figura (Triangulo o Rectangulo),
+luego llame al constructor adecuado solicitando al usuario los parámetros necesarios, dependiendo del
+tipo de figura.
+• Después de crear la figura, mostrará las dimensiones de la misma (sea cual sea su tipo).
+• Finalmente, y utilizando el operador instanceof, mostrará unos datos adicionales dependiendo del
+tipo de figura que sea:
+- Si es de tipo Triangulo, mostrará el área.
+- Si es de tipo Rectangulo, mostrará si es de forma cuadrada o no.
+• Usar una única variable para almacenar la figura, sea del tipo que sea.
+ */
+
 public class Ejercicio2 {
     
 }
