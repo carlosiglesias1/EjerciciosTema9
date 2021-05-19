b3e08d8763e21f4df9bd717af01a253eeac3bb76
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-05-19 20:05:06 +0200

    JODER

--- src/main/java/ejercicios/Ejercicio2.java
+++ src/main/java/ejercicios/Ejercicio2.java
@@ -6,30 +6,31 @@ import java.util.Scanner;
 import recursos.figuras.*;
 
 /**
- * 9.2. Realiza un programa que permita al usuario seleccionar un tipo de figura (Triangulo o Rectangulo),
-luego llame al constructor adecuado solicitando al usuario los parámetros necesarios, dependiendo del
-tipo de figura.
-• Después de crear la figura, mostrará las dimensiones de la misma (sea cual sea su tipo).
-• Finalmente, y utilizando el operador instanceof, mostrará unos datos adicionales dependiendo del
-tipo de figura que sea:
-- Si es de tipo Triangulo, mostrará el área.
-- Si es de tipo Rectangulo, mostrará si es de forma cuadrada o no.
-• Usar una única variable para almacenar la figura, sea del tipo que sea.
+ * 9.2. Realiza un programa que permita al usuario seleccionar un tipo de figura
+ * (Triangulo o Rectangulo), luego llame al constructor adecuado solicitando al
+ * usuario los parámetros necesarios, dependiendo del tipo de figura. • Después
+ * de crear la figura, mostrará las dimensiones de la misma (sea cual sea su
+ * tipo). • Finalmente, y utilizando el operador instanceof, mostrará unos datos
+ * adicionales dependiendo del tipo de figura que sea: - Si es de tipo
+ * Triangulo, mostrará el área. - Si es de tipo Rectangulo, mostrará si es de
+ * forma cuadrada o no. • Usar una única variable para almacenar la figura, sea
+ * del tipo que sea.
  */
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        ArrayList <Figura2D_v8> figuraList = new ArrayList<Figura2D_v8>();
+        ArrayList<Figura2D_v8> figuraList = new ArrayList<Figura2D_v8>();
         char opcion;
         Scanner teclado = new Scanner(System.in);
         do {
             opcion = teclado.nextLine().charAt(0);
-            switch (opcion){
+            switch (opcion) {
                 case 't':
-                    figuraList.add(new Triangulo_v8(Float.parseFloat(teclado.nextLine()),"rectángulo"));
+                    figuraList.add(new Triangulo_v8(Float.parseFloat(teclado.nextLine()), "rectángulo"));
                     break;
                 case 'r':
-                    figuraList.add(new Rectangulo_v8(Float.parseFloat(teclado.nextLine()), Float.parseFloat(teclado.nextLine()), "rectángulo"));
+                    figuraList.add(new Rectangulo_v8(Float.parseFloat(teclado.nextLine()),
+                            Float.parseFloat(teclado.nextLine()), "rectángulo"));
                     break;
                 default:
                     figuraList.add(new Figura2D_v8());
@@ -37,11 +38,10 @@ public class Ejercicio2 {
             }
         } while (opcion != 's');
         for (Figura2D_v8 figura2d_v8 : figuraList) {
-            if (figura2d_v8 instanceof Triangulo_v8){
+            if (figura2d_v8 instanceof Triangulo_v8) {
                 Triangulo_v8 triangulo_v8 = (Triangulo_v8) figura2d_v8;
                 System.out.println(triangulo_v8.area());
-            }
-            else if (figura2d_v8 instanceof Rectangulo_v8){
+            } else if (figura2d_v8 instanceof Rectangulo_v8) {
                 Rectangulo_v8 rectangulo_v8 = (Rectangulo_v8) figura2d_v8;
                 if (rectangulo_v8.esCuadrado()) {
                     System.out.println("Es cuadrado");
