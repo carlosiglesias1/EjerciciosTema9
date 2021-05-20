e092a46b5eb4a6f119efe56d8792795a9edc5c54
carlos <carlosig490@gmail.com>
2021-05-20 23:08:22 +0200

    nuevo 10

---
+++ src/main/java/ejercicios/Ejercicio10.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+/**
+ * 9.10. Haz un programa con un menú que permita gestionar un parking. • El
+ * parking tiene 100 plazas y pueden aparcar 3 tipos de vehículos distintos:
+ * Coches, Furgonetas y Autobuses. • Todos los vehículos pagan 4 céntimos por
+ * minuto, pero las furgonetas pagan además un suplemento de 20 céntimos por
+ * cada metro de su longitud y los autobuses pagan un suplemento de 25 céntimos
+ * por asiento. • El menú del programa deberá permitir: a) Entrada de un
+ * vehículo. Se le pide al usuario la matrícula, tipo de vehículo y datos
+ * adicionales para el cálculo de la estancia (longitud, número de asientos…).
+ * b) Salida del vehículo. Se le pide al usuario la matrícula, se calcula el
+ * importe a pagar y libera la plaza. c) Mostrar la lista de vehículos en el
+ * parking con la matrícula, tipo de vehículo y fecha/hora de llegada (piensa en
+ * el método toString). Al final número total de plazas ocupadas. d) Salir •
+ * Puedes hacer coste 4 céntimos por segundo (en vez de por minuto) para
+ * probarlo. • No hay el concepto de número de plaza, los coches van aparcando
+ * donde quieren. • Mantén la mayor cantidad de información (datos y cálculos)
+ * en las clases, no en el programa. Puedes crear las clases en el mismo archivo
+ * que el programa (por comodidad) con el modificador de acceso por defecto. •
+ * El parking será un ArrayList. Para localizar un vehículo (en la opción de
+ * menú de Salida del vehículo) emplea ArrayList.indexOf y ello te puede
+ * implicar definir equals en alguna clase.
+ */
+
+public class Ejercicio10 {
+
+}
