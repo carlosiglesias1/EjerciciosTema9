ac03e9e4ca504be832d2115cb0c4adca0fdea5ca
carlos <carlosig490@gmail.com>
2021-05-21 15:38:10 +0200

    merda cona joder

---
+++ src/main/java/ejercicios/Ejercicio11.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+/**
+ * 9.11. Partiendo de clases del ejercicio 8.9 (CuentaCorriente, CuentaPlazo),
+ * haz un programa con un menú que permita gestionar unas cuentas bancarias que
+ * se almacenan en un ArrayList (añadir cuenta, eliminar cuenta, ingresos y,
+ * retiradas) siempre accediendo por un identificador que tendrá cada cuenta.
+ */
+
+public class Ejercicio11 {
+
+}
